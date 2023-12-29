1afef66b8d7b59b595e73f6ca552ff5d3d34bf51
noyonalways <noyonrahman2003@gmail.com>
2023-12-29 18:23:39 +0600

    update: update accordion

--- bookmark-landing-page/js/script.js
+++ bookmark-landing-page/js/script.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	accordionItems.forEach((item) => {
 		const header = item.querySelector(".accordion-header");
+		const chevronIcon = header.querySelector("i");
 
 		header.addEventListener("click", () => {
 			const content = item.querySelector(".accordion-content");
@@ -12,11 +13,23 @@ document.addEventListener("DOMContentLoaded", function () {
 			accordionItems.forEach((otherItem) => {
 				const otherContent = otherItem.querySelector(".accordion-content");
 				otherContent.style.display = "none";
+				const otherIcon = otherItem.querySelector(".accordion-header i");
+				otherIcon.classList.remove("bx-chevron-up");
+				otherIcon.classList.add("bx-chevron-down");
 			});
 
 			// Toggle the clicked accordion
 			content.style.display = isOpen ? "none" : "block";
 			item.style.border = isOpen ? "1px solid #ddd" : "1px solid #5368df";
+
+			// Toggle the chevron icon
+			if (isOpen) {
+				chevronIcon.classList.remove("bx-chevron-up");
+				chevronIcon.classList.add("bx-chevron-down");
+			} else {
+				chevronIcon.classList.remove("bx-chevron-down");
+				chevronIcon.classList.add("bx-chevron-up");
+			}
 		});
 	});
 });
